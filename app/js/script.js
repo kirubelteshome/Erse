2b7ab42135f1a9ed14a3bc5357c78a161a2a672d
kirubelteshome <60772835+kirubelteshome@users.noreply.github.com>
2022-08-04 23:55:26 -0700

    created productspage

--- app/js/script.js
+++ app/js/script.js
@@ -97,3 +97,33 @@ var swiperProduct = new Swiper(".product__container", {
     },
   },
 });
+
+
+// product page
+const colorBtns = document.querySelectorAll(".color");
+colorBtns.forEach((color) => {
+  color.addEventListener("click", () => {
+    let colorNameClass = color.className;
+    if (!color.classList.contains("active-color")) {
+      let colorName = colorNameClass.slice(
+        colorNameClass.indexOf("-") + 1,
+        colorNameClass.length
+      );
+      resetActiveBtns();
+      color.classList.add("active-color");
+      setNewColor(colorName);
+    }
+  });
+});
+
+// resetting all color btns
+function resetActiveBtns(){
+  colorBtns.forEach(color => {
+      color.classList.remove('active-color');
+  });
+}
+
+// set new color on the banner right 
+function setNewColor(color){
+  document.querySelector('.right').src = `images/sofa_${color}.png`;
+}
